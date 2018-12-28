bd8d0c6e20ba7b2be2777975f38614b8dbdae39b
Marouan <elhajjaji.marouan@gmail.com>
2018-12-28 20:07:24 +0100

    Ajout du http loader

--- factory-front/src/app/app.module.ts
+++ factory-front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgHttpLoaderModule } from 'ng-http-loader';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,6 +31,7 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgHttpLoaderModule.forRoot(),
     FormsModule,
     AngularFontAwesomeModule,
     NgbModule.forRoot()
